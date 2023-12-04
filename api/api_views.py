caf5b92d0a4f3ae4e66e2f84e10c747b177ee8dd
Flowmikro <cloplop091@gmail.com>
2023-12-04 23:34:45 +0300

    Реализовал добавление расходов, черновой вариант

---
+++ api/api_views.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+
+import config_api
+from .schemas import ExpensesSchema
+from .models import ExpensesModel
+
+user_router = APIRouter()
+
+
+@user_router.post('/', response_model=ExpensesSchema)
+async def create_user(user: ExpensesSchema, session: AsyncSession = Depends(config_api.get_session)):
+    valid = ExpensesSchema(**user.dict())
+    db_user = ExpensesModel(**valid.dict())
+    session.add(db_user)
+    await session.commit()
+    raise HTTPException(status_code=201, detail='Пользователь создан')
+
